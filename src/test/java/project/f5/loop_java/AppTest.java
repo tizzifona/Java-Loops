bc87bf6aba2e24835d107be68663f049782c8043
tizzifona <tizzifona@gmail.com>
2024-10-29 10:30:34 +0100

    Added structure of a project

---
+++ src/test/java/project/f5/loop_java/AppTest.java
@@ -0,0 +1,18 @@
+package project.f5.loop_java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}

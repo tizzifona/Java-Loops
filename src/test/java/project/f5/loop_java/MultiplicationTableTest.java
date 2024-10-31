495dca56de40da31ddd1305192cef6a0f3f60f5f
tizzifona <tizzifona@gmail.com>
2024-10-31 10:59:06 +0100

    Added test of MultiplicationTable for 5

--- src/test/java/project/f5/loop_java/MultiplicationTableTest.java
+++ src/test/java/project/f5/loop_java/MultiplicationTableTest.java
@@ -1,5 +1,33 @@
 package project.f5.loop_java;
 
-public class MultiplicationTableTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class MultiplicationTableTest {
+    @Test
+    public void testMultiplicationTableFor5() {
+        MultiplicationTable table = new MultiplicationTable();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        table.printMultiplicationTable(5);
+
+        String expectedOutput = "5 x 1 = 5\n" +
+                "5 x 2 = 10\n" +
+                "5 x 3 = 15\n" +
+                "5 x 4 = 20\n" +
+                "5 x 5 = 25\n" +
+                "5 x 6 = 30\n" +
+                "5 x 7 = 35\n" +
+                "5 x 8 = 40\n" +
+                "5 x 9 = 45\n" +
+                "5 x 10 = 50\n";
+
+        assertThat(outputStream.toString(), is(equalTo(expectedOutput)));
+    }
 }
